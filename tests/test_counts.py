7a053b894d60cedf5425f88ab0ac6319e7db5021
UserDev987 <UserDev987@gmail.com>
2022-08-14 21:02:22 +0200

    fixed something

--- tests/test_counts.py
+++ tests/test_counts.py
@@ -1,4 +1,4 @@
-from src.main import count_ints, count_letters, count_symbols
+from main import count_ints, count_letters, count_symbols
 
 def test_count_ints():
     assert count_ints("143242") == 6
